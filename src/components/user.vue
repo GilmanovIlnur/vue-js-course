d9223e2dadd3222615f4501cd7e65ae21443feaa
Gilmanov Ilnur <ilnur19982014@mail.ru>
2020-02-15 14:50:50 +0300

    начальная версия

---
+++ src/components/user.vue
@@ -0,0 +1,24 @@
+<template>
+asfasfasfas
+</template>
+
+<script>
+    export default {
+        name: "User",
+        data(){
+            return{
+                text: 'Идет загрузка...',
+                postSeen: false
+            }
+        },
+        methods: {
+            clicked: function () {
+                this.postSeen = !this.postSeen
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
